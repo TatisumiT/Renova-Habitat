9aad1cdf3342ba56a65bcebb107a922c4ae3c720
TatisumiT <163027358+TatisumiT@users.noreply.github.com>
2024-03-11 13:52:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('Enviar');
+const messageElement = document.getElementById('message');
+
+button.addEventListener('click', function Enviar() {
+        alert("Seu login foi registrado com sucesso.");
+});
+
